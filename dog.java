654005df27bb388992b1d8b4ab1e3412a4120bdf
TobiasThalbitzer <73828508+TobiasThalbitzer@users.noreply.github.com>
2022-04-24 22:21:17 +0200

    uploaded

---
+++ dog.java
@@ -0,0 +1,20 @@
+public class dog {
+    String name;
+    int age;
+    String color;
+
+    dog(){System.out.println("initiated dog sequence");
+    }
+
+    System.out.prinln("ed");
+
+    public void setColor(String newColor){
+        color = newColor;
+    }
+
+    
+    public void bark(){
+        System.out.println("Wuff");
+    }
+}
+
---
+++ hello.java
@@ -0,0 +1,17 @@
+
+public class hello
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hej");
+
+    dog minhund = new dog();
+    minhund.setColor("grønd");
+    System.out.println(minhund.color);
+
+    minhund.age = 5;
+    System.out.println(minhund.age);
+
+    }
+}
